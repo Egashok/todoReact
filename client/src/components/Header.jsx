fab0ca0a80fcd84a0e1f7c79227eaab8726378b4
ceyaa <nikitaveremeenko03@gmail.com>
2023-12-17 19:03:17 +0600

    new css

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = ({visSide}) => {
     return ( 
         <header className='header'><div onClick={visSide}  className='header__hamb'>
         <button className='header__btn'>
-            </button></div><h1 className='header__title'>Моя тудушечка</h1>
+            </button></div><h1 className='header__title'>Список дел</h1>
         </header>
          );
 }
