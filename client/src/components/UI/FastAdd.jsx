3c5f60051301eb1a4389ff84b7fedfac0d5b07d1
ceyaa <nikitaveremeenko03@gmail.com>
2023-12-19 23:39:49 +0600

    css fix

--- client/src/components/UI/FastAdd.jsx
+++ client/src/components/UI/FastAdd.jsx
@@ -23,7 +23,6 @@ const Creator = ({visChange,fastAdd,setFastAdd}) => {
         <div className='creator__top'>
           <input value={task.title} onChange={e=>setTask({...task,title:e.target.value})} placeholder='Название задачи' className='creator__name'/>
           <input  value={task.time} onChange={e=>setTask({...task,time:e.target.value})}  placeholder='00:00' className='creator__time'/>
-          <input  value={task.date} onChange={e=>setTask({...task,date:e.target.value})}  placeholder='21.12.2023' className='creator__time'/>
         </div>
         <div className='creator__bottom'>
           <input  value={task.body} onChange={e=>setTask({...task,body:e.target.value})}  placeholder='Описание задачи' className='creator__body'/>
